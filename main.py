5c2bb9342bb0af00617e76045ba6f1352be1807e
Kevin Garner <kg.dev@protonmail.com>
2020-10-18 19:17:07 -0500

    variable name change

--- main.py
+++ main.py
@@ -3,34 +3,34 @@ import config_secret as config
 # If you're using this code from GitHub, remove the line above, and uncomment the line below.
 #import config
 
-def on_open_connection(ws):
+def on_open_connection(websock):
     print("Connection opened!")
 
     auth_data = {"action" : "authenticate",
                  "data" : {"key_id" : config.KEY_ID, "secret_key": config.SECRET_KEY}
                  }
-    ws.send(json.dumps(auth_data))
+    websock.send(json.dumps(auth_data))
     # listen for Tesla and Apple stocks at once a minute
     listen_message = {"action":"listen", "data":{"streams":["AM.TSLA", "AM.AAPL"]}}
     # listen for Tesla stock at tick level
     #listen_message = {"action":"listen", "data":{"streams":["T.TSLA"]}}
-    ws.send(json.dumps(listen_message))
+    websock.send(json.dumps(listen_message))
 
 
-def on_message_receive(ws, message):
+def on_message_receive(websock, message):
     print("Message:")
     print(message)
 
     # deal with the message here, i.e. buy/sell stock, perform analysis, etc.
 
-def on_close_connection(ws):
+def on_close_connection(websock):
     print("Connection closed.")
 
 
 socket = "wss://data.alpaca.markets/stream"
 
-ws = websocket.WebSocketApp(socket,
+websock = websocket.WebSocketApp(socket,
                             on_open=on_open_connection,
                             on_message=on_message_receive,
                             on_close=on_close_connection)
-ws.run_forever()
+websock.run_forever()
